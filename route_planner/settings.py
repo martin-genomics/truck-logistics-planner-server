6c743db38111d6b2ec5284e8440dd35cd1f10a48
martin-genomics <martintembo.zm@gmail.com>
2025-09-17 15:01:31 +0200

    updated pg package

--- route_planner/settings.py
+++ route_planner/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'route_planner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'railway',
         'USER': 'postgres',
         'PASSWORD': 'ciyzbPenamdLBQJTvhIAmGgesOURzBJL',
